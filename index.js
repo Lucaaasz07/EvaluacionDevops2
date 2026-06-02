b51ea28ee8b6da961942ee31e8fd23b50795cfac
Lucaaasz07 <luc.pizarroh@duocuc.cl>
2026-06-02 19:17:59 -0400

    Hola mundo

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let notas = [];
 
 app.get("/", (req, res) => {
   res.json({ 
-    mensaje: "Bienvenido Al cambio 1 📝", 
+    mensaje: "Bienvenido Al cambio 1 📝",
+    mensaje: "Hola Mundo",
     version: "1.0.0",
     autor: "Lucas"
   });
